6415f73b218be762bb5d2c55683c9b887223109b
thomasgeiger11 <36414689+thomasgeiger11@users.noreply.github.com>
2019-11-23 13:40:45 +0100

    graphiques

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Graphique from './graphiques.js'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <Graphique />
     </div>
   );
 }
---
+++ frontend/src/graphiques.js
@@ -0,0 +1,68 @@
+import React, { PureComponent } from 'react';
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+const data = [
+  {
+    name: 'Janvier', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: 'Février', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: 'Mars', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: 'Avril', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: 'Mai', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: 'Juin', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: 'Juillet', uv: 3490, pv: 4300, amt: 2100,
+  },
+  {
+    name: 'Août', uv: 3490, pv: 4300, amt: 2100,
+  },
+  {
+    name: 'Septembre', uv: 3490, pv: 4300, amt: 2100,
+  },
+  {
+    name: 'Octobre', uv: 3490, pv: 4300, amt: 2100,
+  },
+  {
+    name: 'Novembre', uv: 3490, pv: 4300, amt: 2100,
+  },
+  {
+    name: 'Décembre', uv: 3490, pv: 4300, amt: 2100,
+  },
+];
+
+export default class Graphique extends PureComponent {
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
+
+  render() {
+    return (
+      <LineChart
+        width={1500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    );
+  }
+}
